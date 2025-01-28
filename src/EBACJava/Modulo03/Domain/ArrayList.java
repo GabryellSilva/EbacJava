6f449004d2075d9049fdefaf1b19b880211674cc
GabryellSilva <gabryell568silva@gmail.com>
2025-01-28 12:38:43 -0300

    Aula sobre coleções em Java

---
+++ src/EBACJava/Modulo03/Domain/ArrayList.java
@@ -0,0 +1,32 @@
+package EBACJava.Modulo03.Domain;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class ArrayList<S> {
+    public static void main(String[] args) {
+    exemplosDeListasSimples();
+    exemploDeListaOrdenadaAscendida();
+    }
+    public static void exemplosDeListasSimples(){
+        System.out.println("========== Exemplo de listas simples =====================");
+        List<String> lista = new java.util.ArrayList<>();
+        lista.add("João");
+        lista.add("Maria");
+        lista.add("Roberto");
+        System.out.println(lista);
+        System.out.println("");
+    }
+
+    public static void exemploDeListaOrdenadaAscendida(){
+        System.out.println("========== Exemplo de listas Ordenada Ascendida =====================");
+        List<String> lista2 = new java.util.ArrayList<>();
+        lista2.add("Gabryell");
+        lista2.add("Layane");
+        lista2.add("Ana");
+        Collections.sort(lista2);
+        System.out.println(lista2);
+        System.out.println("");
+    }
+}
